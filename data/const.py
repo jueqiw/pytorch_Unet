eca7234ac9b1e19e5e717e661f0fa3c96c01bca7
wendy-xiaozong <543443859@qq.com>
2020-06-04 10:00:46 -0300

    the example model

--- data/const.py
+++ data/const.py
@@ -18,7 +18,7 @@ LEARN_RATE = 0.001
 #
 # ADNI_LABEL = "brain_extraction"
 
-DATA_ROOT = Path(__file__).resolve().parent.parent.parent.parent
+DATA_ROOT = Path(__file__).resolve().parent.parent / "data"
 
 print(DATA_ROOT)
 
