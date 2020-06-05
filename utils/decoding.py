08b1195d33241a4409688b6ddfa8c09194f18e2b
wendy-xiaozong <543443859@qq.com>
2020-06-05 20:35:56 -0300

    the example model

--- utils/decoding.py
+++ utils/decoding.py
@@ -137,17 +137,17 @@ class DecodingBlock(nn.Module):
         return x
 
     def center_crop(self, skip_connection, x):
-        # c = (bypass.size()[2] - upsampled.size()[2]) // 2
-        # bypass = F.pad(bypass, (-c, -c, -c, -c))
+        c = (skip_connection.size()[2] - x.size()[2]) // 2
+        bypass = F.pad(skip_connection, [-c, -c, -c, -c])
 
-        skip_shape = torch.tensor(skip_connection.shape)
-        x_shape = torch.tensor(x.shape)
-        crop = skip_shape[2:] - x_shape[2:]
-        half_crop = crop // 2
-        # If skip_connection is 10, 20, 30 and x is (6, 14, 12)
-        # Then pad will be (-2, -2, -3, -3, -9, -9)
-        pad = -torch.stack((half_crop, half_crop)).t().flatten()
-        skip_connection = F.pad(skip_connection, pad.tolist())
+        # skip_shape = torch.tensor(skip_connection.shape)
+        # x_shape = torch.tensor(x.shape)
+        # crop = skip_shape[2:] - x_shape[2:]
+        # half_crop = crop // 2
+        # # If skip_connection is 10, 20, 30 and x is (6, 14, 12)
+        # # Then pad will be (-2, -2, -3, -3, -9, -9)
+        # pad = -torch.stack((half_crop, half_crop)).t().flatten()
+        # skip_connection = F.pad(skip_connection, pad.tolist())
         return skip_connection
 
 
