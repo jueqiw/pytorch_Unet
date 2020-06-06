7bf9e72f8b6cae95d6b07fb223777d955c49c433
wendy-xiaozong <543443859@qq.com>
2020-06-06 10:58:32 -0300

    the example model

--- utils/decoding.py
+++ utils/decoding.py
@@ -140,7 +140,7 @@ class DecodingBlock(nn.Module):
     def forward(self, skip_connection, x):
         x = self.upsample(x)  # upConvLayer
         skip_connection = self.center_crop(skip_connection, x)  # Got 35 and 34
-        print(skip_connection.shape, x)
+        print(skip_connection.shape, x.shape)
         # skip_connection = x
         x = torch.cat((skip_connection, x), dim=CHANNELS_DIMENSION)
         # print(f"skip connection: {skip_connection.shape}")
