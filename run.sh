c4ca86a55001939e6a59cf1ed088486f3bc87c89
wendy-xiaozong <543443859@qq.com>
2020-06-08 20:54:08 -0300

    add batch information print

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:t4:1  # request GPU "generic resource"
 #SBATCH --cpus-per-task=16   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=170G   # memory
-#SBATCH --output=oneclasses-%j.out  # %N for node name, %j for jobID
+#SBATCH --output=cc359-%j.out  # %N for node name, %j for jobID
 #SBATCH --time=03-00:00      # time (DD-HH:MM)
 #SBATCH --mail-user=x2019cwn@stfx.ca # used to send email
 #SBATCH --mail-type=ALL
